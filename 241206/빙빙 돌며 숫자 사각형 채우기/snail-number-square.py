6e4b6ac30d8abd9a6d27d8fd2ed6b44ef43e8148
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-06 23:03:09 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 5012ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 241206/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -0,0 +1,45 @@
+dxs, dys =  [0,1,0,-1], [1,0,-1,0]
+
+def in_range(x, y):
+    return 1 <= x and x <= r and 1 <= y and y <= c
+'''
+'''
+
+r, c = map(int, input().split())
+
+a = []
+for j in range(r):
+    temp = []
+    for i in range(c):
+        temp.append(0)
+    a.append(temp)
+# print(a)
+
+
+'''
+1,6 (0,5)
+2,6 (1,5)
+
+'''
+
+x, y = 1, 1
+
+cnt = 1
+a[x-1][y-1] = cnt
+cnt += 1
+
+print(a)
+
+dir_num = 0
+
+while True:
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+    if in_range(nx, ny) and a[nx-1][ny-1] != 0:
+        a[nx-1][ny-1] = cnt
+        cnt += 1
+        if cnt > r * c:
+            break
+    else:
+        dir_num = (dir_num+1)%4
+
+print(a)
