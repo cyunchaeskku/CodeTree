d6b07fe38955c596828e47a8e40538d9e4204cc6
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-07 19:39:10 +0900

    Title: 되돌아오기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241207/되돌아오기/come-back.py
@@ -0,0 +1,26 @@
+dxs, dys = [1,0,-1,0], [0,-1,0,1]
+
+d = {}
+
+d['E'] = 0
+d['S'] = 1
+d['W'] = 2
+d['N'] = 3
+
+n = int(input())
+
+x, y = 0, 0 
+ans = 0
+
+for i in range(n):
+    dir_str, dist = input().split()
+    dist = int(dist)
+    dir_num = d[dir_str]
+    
+    for j in range(dist):
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+        ans += 1
+        if x == 0 and y == 0:
+            print(ans)
+            break
+    
