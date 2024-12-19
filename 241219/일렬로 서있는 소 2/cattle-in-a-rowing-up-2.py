56fcd0eafc453c1ecbb874e079d3829b3e23f442
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-19 20:04:09 +0900

    Title: 일렬로 서있는 소 2, Time: 70ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241219/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 241219/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -7,7 +7,7 @@ cnt = 0
 
 for i in range(n-2):
     for j in range(i+1, n-1):
-        for k in rnage(j+1, n):
+        for k in range(j+1, n):
             if a[i] <= a[j] and a[j] <= a[k]:
                 cnt += 1
 
