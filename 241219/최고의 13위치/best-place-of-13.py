8fc48bb2f40fcb54e89fb59fd54617ff13099646
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-19 20:16:17 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241219/최고의 13위치/best-place-of-13.py
+++ 241219/최고의 13위치/best-place-of-13.py
@@ -8,10 +8,11 @@ for i in range(n):
     temp = list(map(int,input().split()))
     a.append(temp)
 
+
 max_cnt = -sys.maxsize
 
 for i in range(n):
     for j in range(n-2):
-        max_cnt = max(max_cnt, a[i][j] + a[i][j+1], a[i][j+2])
+        max_cnt = max(max_cnt, a[i][j] + a[i][j+1] + a[i][j+2])
 
 print(max_cnt)
