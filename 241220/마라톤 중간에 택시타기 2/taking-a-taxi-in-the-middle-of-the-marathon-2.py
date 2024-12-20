f5cf43864367528529f098ef31ad429279d2cd46
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-20 19:48:46 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241220/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 241220/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -14,7 +14,6 @@ for i in range(n):
 for i in range(1, n-1):
     temp_list = a.copy()
     del temp_list[i]
-    print(temp_list)
     length = 0
     for j in range(1, len(temp_list)):
         length += abs( temp_list[j][0] - temp_list[j-1][0] ) + abs( temp_list[j][1] - temp_list[j-1][1] )
