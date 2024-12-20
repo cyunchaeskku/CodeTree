e3bbfd6ab88a13d8c05756fbb2efae16f18b6b6d
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-20 18:58:52 +0900

    Title: 체크판위에서 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241220/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,29 @@
+r, c = map(int, input().split())
+
+a = []
+
+for i in range(r):
+    a.append( list(input().split()) ) 
+
+ans = 0
+
+'''
+0 = True
+1 = False
+'''
+cur_color = -1
+
+if a[0][0] == 'W':
+    cur_color = True
+else:
+    cur_color = False
+
+for i in range(1,r-1):
+    for j in range(c-1):
+        for k in range(i+1, r):
+            for l in range(j+1, c):
+                if a[i][j] != a[0][0] and a[k][l] == a[0][0] and k < r-1 and l < c-1:
+                    # print(a[i][j], a[k][l], i+1,j+1,k+1,l+1)
+                    ans += 1
+
+print(ans)
