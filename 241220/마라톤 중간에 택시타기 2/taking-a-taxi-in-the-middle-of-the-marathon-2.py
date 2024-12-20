83bcdcd94268d23162c147050045ff79130a8501
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-20 19:48:17 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241220/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,25 @@
+import sys
+
+min_ans = sys.maxsize
+
+n = int(input())
+
+a= []
+
+for i in range(n):
+    a.append( list(map(int,input().split())) )
+
+# length = abs( a[j][0] - a[j-1][0] ) + abs( a[j][1] - a[j-1][1] )
+
+for i in range(1, n-1):
+    temp_list = a.copy()
+    del temp_list[i]
+    print(temp_list)
+    length = 0
+    for j in range(1, len(temp_list)):
+        length += abs( temp_list[j][0] - temp_list[j-1][0] ) + abs( temp_list[j][1] - temp_list[j-1][1] )
+    if length < min_ans:
+        min_ans = length
+
+
+print(min_ans)            
