fa7e55a227c6e5eebb05da32a46a1d977ea4127c
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-21 09:40:35 +0900

    Title: 원 모양으로 되어있는 방, Time: 74ms, Memory: 17MB, Status: Passed - CodeTree

--- 241221/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 241221/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -9,13 +9,24 @@ for i in range(n):
 
 min_length = sys.maxsize
 
+cnt = 0
+
 for i in range(n): # i에서 시작한다면
     length = 0
+    cnt = 0
     # print("i:", i)
+    # for j in range(0, i):
+    #     length += a[j] * (n - j - 1)
+    # for j in range(i,n):
+    #     length += a[j] * (j - i)
+
+    for j in range(i, n):
+        length += a[j] * cnt
+        cnt += 1
     for j in range(0, i):
-        length += a[j] * (n - j - 1)
-    for j in range(i,n):
-        length += a[j] * (j - i)
+        # print(cnt)
+        length += a[j] * cnt
+        cnt += 1
 
     min_length = min(min_length, length)
     # print(length)
@@ -28,12 +39,12 @@ a[1] * (1 - i) 0
 a[2] * (2 - i) 1
 a[3] * (3 - i) 2
 a[4] * (4 - i) 3
-a[0] * (n - 0 -1) 4
+a[0] * (n - 0 - 1) 4
 
 i = 2
 a[2] * (2 - i) 0
 a[3] * (3 - i) 1
 a[4] * (4 - i) 2
-a[0] * (n - 0 - 1)
-a[1] * (n - 1 - 1)
+a[0] * (n - 0 - 1) 4
+a[1] * (n - 1 - 1) 3
 '''
