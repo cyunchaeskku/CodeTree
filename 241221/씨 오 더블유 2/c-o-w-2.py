015964768acfcae476289ce0c2829c42abf3c483
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-21 09:54:02 +0900

    Title: 씨 오 더블유 2, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241221/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+string = list(input().rstrip())
+
+cnt = 0
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            if string[i] == 'C' and string[j] == 'O' and string[k] == 'W':
+                cnt += 1
+
+
+print(cnt)
