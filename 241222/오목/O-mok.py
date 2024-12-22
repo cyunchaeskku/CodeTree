fae79b29eea99c7202df55590cec2663a18be098
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-22 15:30:29 +0900

    Title: 오목, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241222/오목/O-mok.py
@@ -0,0 +1,40 @@
+import sys
+
+drs, dcs = [0,1,0,-1],[1,0,-1,0]
+
+def in_range(r, c):
+    return 0 <= r and r < 19 and 0 <= c and c < 19
+
+a = []
+
+for i in range(19):
+    for j in range(19):
+        a.append( list(map(int,sys.stdin.readline().rstrip().split()) ) )
+
+win = 0 # 0:tie, 1: black win, 2: white win
+dir_num = -1
+
+for i in range(19):
+    for j in range(19):
+        if a[i][j] != 0:
+            for k in range(4):
+                ni, nj = i + drs[k], j + dcs[k]
+                if a[ni][nj] == a[i][j]:
+                    dir_num = k
+                    break
+            r, c = i, j
+            cnt = 0
+            for k in range(5):
+                nr, nc = r + drs[dir_num], c + dcs[dir_num]
+                if a[nr][nc] == a[r][c]:
+                    r, c = nr, nc
+                    cnt += 1
+            if cnt == 4:
+                print(a[r][c])
+                dir_num = (dir_num+2)%4
+                nr, nc = r + 2 * drs[dir_num], c + 2 * dcs[dir_num]
+                print(nr+1, nc+1)
+                exit(0)
+
+
+
