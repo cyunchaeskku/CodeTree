447ecfee25238b672d8fc54689abbfca5c606d0b
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-22 16:06:01 +0900

    Title: 최고의 13위치 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241222/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(input())
+
+a = []
+
+max_ans = -sys.maxsize
+
+for i in range(n):
+    a.append( list(map(int, input().split())) )
+
+for i in range(n-1):
+    for j in range(n-2):
+        for k in range(i+1, n):
+            for l in range(n-2):
+                sum = a[i][j] + a[i][j+1] + a[i][j+2] + a[k][l] + a[k][l+1] + a[k][l+2]
+                max_ans = max (max_ans, sum)
+
+            
+print(max_ans)
