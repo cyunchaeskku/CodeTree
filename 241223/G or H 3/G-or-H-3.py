2887e2f401b1c1cbb6b2c79fafd88b4a158ee079
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-23 19:37:06 +0900

    Title: G or H 3, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241223/G or H 3/G-or-H-3.py
@@ -0,0 +1,26 @@
+import sys
+
+max_ans = -sys.maxsize
+
+n, k = map(int, input().split())
+
+a = [0 for _ in range(10_000)]
+
+for i in range(n):
+    index, alphabet = input().split()
+    a[int(index) - 1] = alphabet
+
+for i in range(n - k + 1):
+    sum = 0
+    for j in range(i, i + k + 1):
+        if a[j] == 'G':
+            sum += 1
+        if a[j] == 'H':
+            sum += 2
+    max_ans = max(max_ans, sum)
+
+print(max_ans)
+'''
+8 9
+10 - 1
+'''
