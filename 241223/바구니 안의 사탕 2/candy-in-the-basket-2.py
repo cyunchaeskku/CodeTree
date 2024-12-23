e2e88ac08c5158aa1615f1cc6e22b9a62a93b4f2
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-23 20:20:37 +0900

    Title: 바구니 안의 사탕 2, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 241223/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 241223/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,7 +1,7 @@
 import sys
 
 n , k = map(int, input().split())
-a= [0 for _ in range(200)]
+a= [0 for _ in range(401)]
 
 max_ans = -sys.maxsize
 
@@ -9,7 +9,7 @@ for i in range(n):
     candy, index = map(int, input().split())
     a[index - 1] += candy
 
-for i in range(0 + k ,200 - k):
+for i in range(0 + k ,401 - k):
     _sum = sum(a[i - k : i + k + 1])
     max_ans = max(max_ans, _sum)
 
@@ -20,7 +20,7 @@ print(max_ans)
 
 '''
 c=200
-0~200
+0~400
 
 0 1 2 3 ... 196 197 198 199
 '''
