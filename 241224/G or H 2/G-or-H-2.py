42db3fc48fe1a35d140679b2dc0c4aa20fdfa269
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-24 20:02:19 +0900

    Title: G or H 2, Time: 67ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241224/G or H 2/G-or-H-2.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(input())
+
+a= [0 for _ in range(100)]
+
+for i in range(n):
+    index, alphabet = input().split()
+    a[int(index)-1] = alphabet
+
+max_size = 0
+
+for i in range(100):
+    for j in range(i, 100):
+        a_subarr = a[i:j+1]
+        G_count = 0
+        H_count = 0
+        for v in a_subarr:
+            if v == 'G':
+                G_count += 1
+            if v == 'H':
+                H_count += 1
+        if G_count == 0 or H_count == 0 or G_count == H_count:
+            # print(j-i)
+            max_size = max(max_size, j - i)
+
+print(max_size)
