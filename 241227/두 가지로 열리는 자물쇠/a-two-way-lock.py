c1812f5f909c0da0151d19372e9365cb3fe7bba3
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-27 18:18:05 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 58ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241227/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,31 @@
+import sys
+
+n = int(input())
+
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+cnt = 0
+
+def change(a):
+    if a == n:
+        return 0
+    else:
+        return a
+
+'''
+min(i-a1, n-(i-a1))
+'''
+
+def get_min(i, a1):
+    return min(abs(i-a1),abs(n-(i-a1))) <= 2
+
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):
+            if ( get_min(i, a1) and get_min(j, b1) and get_min(k, c1) ) or ( get_min(i, a2) and get_min(j, b2) and get_min(k, c2) ):
+                cnt += 1
+    
+            
+            
+print(cnt)
