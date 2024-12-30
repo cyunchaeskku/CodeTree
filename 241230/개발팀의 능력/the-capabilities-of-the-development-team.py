a14818592d1067112383bf1c06faf7301d4003a3
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-30 20:04:50 +0900

    Title: 개발팀의 능력, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241230/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,26 @@
+import sys
+
+min_ans = sys.maxsize
+
+a = list(map(int, input().split()))
+
+def get_diff(i,j,k):
+    sum1 = a[i] + a[j]
+    sum2 = a[k]
+    sum3 = sum(a) - sum1 - sum2
+
+    return max(sum1, sum2, sum3) - min(sum1, sum2, sum3)
+
+
+for i in range(len(a)):
+    for j in range(len(a)):
+        for k in range(len(a)):
+            if i == j or j ==k or k == i:
+                continue
+
+            if a[i] + a[j] == a[k] or a[k] == sum(a) - (a[i] + a[j]) -  a[k] or sum(a) - (a[i] + a[j]) -  a[k] == a[i] + a[j]:
+                continue
+
+            min_ans = min(min_ans, get_diff(i,j,k))
+
+print(min_ans)
