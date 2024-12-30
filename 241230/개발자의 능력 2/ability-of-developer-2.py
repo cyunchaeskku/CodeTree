df7b7cc3c81d250cbf3420d13a815ee9122978de
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-12-30 19:57:03 +0900

    Title: 개발자의 능력 2, Time: 56ms, Memory: 17MB, Status: Passed - CodeTree

--- 241230/개발자의 능력 2/ability-of-developer-2.py
+++ 241230/개발자의 능력 2/ability-of-developer-2.py
@@ -19,11 +19,7 @@ for i in range(len(a)):
     for j in range(len(a)):
         for k in range(len(a)):
             for l in range(len(a)):
-                if i != j and j != k and k != l and k != i:
-                    # sum1 = a[i] + a[j]
-                    # sum2 = a[k] + a[l]
-                    # sum3 = sum(a) - sum1 - sum2
-                    # print(max(sum1, sum2, sum3) , min(sum1, sum2, sum3))
+                if i != j and i != k and i != l and j != i and j != k and j != l and k != i and k != j and k != l:
                     # if get_diff(i,j,k,l) == 0:
                     #     print(a[i],a[j],a[k],a[l])
                     min_ans = min( get_diff(i,j,k,l), min_ans )
