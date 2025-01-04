0fb812d4c7a97b4418bc5e1c9a3b3fab33fb7a7b
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-04 12:06:36 +0900

    Title: 데이터센터의 온도 조정 2, Time: 83ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250104/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,33 @@
+import sys
+
+n, c, g, h = map(int, input().split())
+
+tAs = []
+tBs = []
+
+ans = -sys.maxsize
+
+min_temp, max_temp = sys.maxsize, -sys.maxsize
+
+for _ in range(n):
+    ta, tb = map(int, input().split())
+    tAs.append(ta)
+    tBs.append(tb)
+
+    min_temp = min(min_temp, ta)
+    max_temp = max(max_temp, tb)
+
+for temp in range(min_temp-1, max_temp+2):
+    work = 0
+    
+    for i in range(n):
+        if temp < tAs[i]:
+            work += c
+        if temp >= tAs[i] and temp < tBs[i]:
+            work += g
+        if temp >= tBs[i]:
+            work += h
+
+    ans = max(ans, work)
+
+print(ans)
