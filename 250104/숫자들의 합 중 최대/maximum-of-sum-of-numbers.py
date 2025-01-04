691c636789867e6bea69a1a23c84e00b43b190ea
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-04 11:58:03 +0900

    Title: 숫자들의 합 중 최대, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

--- 250104/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 250104/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -5,6 +5,6 @@ ans = -sys.maxsize
 x, y = map(int, input().split())
 
 for n in range(x, y+1):
-    d1, d2 = tuple(map(int, list(str(n))))
-    ans = max(ans, d1 + d2)
+    a = tuple(map(int, list(str(n))))
+    ans = max(ans, sum(a))
 print(ans)
