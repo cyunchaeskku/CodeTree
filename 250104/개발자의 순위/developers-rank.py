a806f86a910425d8dab8e24638720f028d0cc505
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-04 10:20:14 +0900

    Title: 개발자의 순위, Time: 85ms, Memory: 19MB, Status: Runtime Error - CodeTree

--- 250104/개발자의 순위/developers-rank.py
+++ 250104/개발자의 순위/developers-rank.py
@@ -13,8 +13,8 @@ for i in range(1, n+1):
         for j in range(n):
             if v[j] == i:
                 i_index = j
-        if i_index + 1 < n-1:
-            for j in range(i_index+1, n):
+        for j in range(n):
+            if j > i_index:
                 nums[i][v[j]] += 1
 
 
