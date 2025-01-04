b8e85c0d4541b970369212dead13d887147d24cb
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-04 10:21:30 +0900

    Title: 개발자의 순위, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250104/개발자의 순위/developers-rank.py
+++ 250104/개발자의 순위/developers-rank.py
@@ -3,6 +3,7 @@ import sys
 k, n = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(k)]
 
+
 # Write your code here!
 
 nums = [[0 for _ in range(n)] for _ in range(n + 1)]
@@ -15,7 +16,7 @@ for i in range(1, n+1):
                 i_index = j
         for j in range(n):
             if j > i_index:
-                nums[i][v[j]] += 1
+                nums[i][v[j]-1] += 1
 
 
 ans = 0
