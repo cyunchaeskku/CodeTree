cb6e1c194b42528f18baaad49e5cd3b055d8c463
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-05 19:11:50 +0900

    Title: 등차수열, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250105/등차수열/arithmetic-sequence.py
+++ 250105/등차수열/arithmetic-sequence.py
@@ -7,12 +7,11 @@ a = list(map(int, input().split()))
 min_val = min(a)
 max_val = max(a)
 
-ans = 0
 
 for k in range(1, 101): # from 1 to 100
+ans = 0
     for i in range(len(a)):
         for j in range(i+1, len(a)):
-
             if (k - a[i] == a[j] - k) and k != a[i] and k != a[j]:
                 ans +=1
 print(ans)
