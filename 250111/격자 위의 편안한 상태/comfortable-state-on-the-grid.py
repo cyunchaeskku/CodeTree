1ff7b90ce46278905a04bf07759d1d2bafd63337
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-11 19:44:10 +0900

    Title: 격자 위의 편안한 상태, Time: 90ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250111/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,26 @@
+import sys
+
+n, m = map(int, input().split())
+
+a = [ [0 for _ in range(n)] for _ in range(n) ]
+
+dxs, dys = [0,1,0,-1], [1,0,-1,0]
+
+def in_range(r, c):
+    return r >= 0 and r < n and c >= 0 and c < n
+
+for i in range(m):
+    r, c = map(int, input().split())
+    r -= 1
+    c -= 1
+    a[r][c] = 1
+    cnt = 0
+    for j in range(4):
+        nr, nc = r + dxs[j], c + dys[j]
+        if in_range(nr, nc) and a[nr][nc] == 1:
+            cnt += 1
+    if cnt >= 3:
+        print(1)
+    else:
+        print(0)
+
