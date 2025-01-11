608e4baa0b99579af8107e2344334d7681577daf
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-11 18:23:27 +0900

    Title: 원소 값들의 최대 합, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -0,0 +1,16 @@
+import sys
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+ans = -sys.maxsize
+
+for i in range(len(a)-1):
+    sum = 0
+    pos = i - 1
+    sum += a[pos]
+    for j in range(m-1):
+        pos = a[pos] - 1
+        sum += a[pos]
+    ans = max(ans, sum)
+
+print(ans)
