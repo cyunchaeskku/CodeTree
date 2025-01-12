4b3341e14c83b66fa42e3c3aa9da86d9f79e6a6e
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-12 15:37:39 +0900

    Title: A, B, C, D 찾기 2, Time: 113ms, Memory: 20MB, Status: Passed - CodeTree

--- 250112/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 250112/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -2,14 +2,48 @@ import sys
 
 arr = list(map(int, input().split()))
 
+
+
 for a in range(min(arr), max(arr) + 1):
     for b in range(a, max(arr) + 1):
         for c in range(b, max(arr) + 1):
             for d in range(c, max(arr) + 1):
-                if a in arr and b in arr and c in arr and d in arr and \
-                a + b in arr and b + c in arr and c + d in arr and d + a in arr and \
-                a + c in arr and b + d in arr and \
-                a + b + c in arr and  a + b + d in arr and a + c + d in arr and b + c + d in arr and \
-                a + b + c + d in arr:
-                    print(a, b, c, d)
-                    break
+                ar = arr.copy()
+                # if a in arr and b in arr and c in arr and d in arr and \
+                # a + b in arr and b + c in arr and c + d in arr and d + a in arr and \
+                # a + c in arr and b + d in arr and \
+                # a + b + c in arr and  a + b + d in arr and a + c + d in arr and b + c + d in arr and \
+                # a + b + c + d in arr:
+                #     print(a, b, c, d)
+                if a in ar:
+                    ar.remove(a)
+                if b in ar:
+                    ar.remove(b)
+                if c in ar:
+                    ar.remove(c)
+                if d in ar:
+                    ar.remove(d)
+                if a+b in ar:
+                    ar.remove(a+b)
+                if b+c in ar:
+                    ar.remove(b+c)
+                if c+d in ar:
+                    ar.remove(c+d)
+                if d+a in ar:
+                    ar.remove(d+a)
+                if a+c in ar:
+                    ar.remove(a+c)
+                if b+d in ar:
+                    ar.remove(b+d)
+                if a+b+c in ar:
+                    ar.remove(a+b+c)
+                if a+b+d in ar:
+                    ar.remove(a+b+d)
+                if a+c+d in ar:
+                    ar.remove(a+c+d)
+                if b+c+d in ar:
+                    ar.remove(b+c+d)
+                if a+b+c+d in ar:
+                    ar.remove(a+b+c+d)
+                if len(ar) == 0:
+                    print(a,b,c,d)
