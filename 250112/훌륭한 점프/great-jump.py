2bfff119c5a899da193013969131fa6f11c6dbe3
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-12 15:05:33 +0900

    Title: 훌륭한 점프, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250112/훌륭한 점프/great-jump.py
@@ -0,0 +1,34 @@
+import sys
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+min_val = min(a)
+max_val = max(a)
+
+ans = sys.maxsize
+
+def is_possible(max_val):
+    available_indicies = []
+    
+    for i in range(n):
+        if a[i] <= max_val:
+            available_indicies.append(i)
+
+    arr_size = len(available_indicies)
+
+    if arr_size == 1:
+        return False
+
+    for i in range(1, arr_size):
+        if available_indicies[i] - available_indicies[i-1] > k:
+            return False
+
+    return True
+
+
+for i in range(1, max_val+1):
+    if is_possible(i):
+        ans = min(ans, i)
+
+print(ans)
