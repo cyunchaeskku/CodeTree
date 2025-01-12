78ef4fd9d57105a991d9b4c5570291b703ebbc64
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-12 19:27:39 +0900

    Title: 이상한 폭탄 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250112/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,34 @@
+import sys
+
+n, k = map(int, input().split())
+
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+bomb_numbers = set()
+for v in arr:
+    bomb_numbers.add(v)
+bomb_numbers = list(bomb_numbers)
+
+ans = 0
+max_boom_count = -sys.maxsize
+
+for v in bomb_numbers:
+    ar = arr.copy()
+    boom_count = 0
+    
+    for i in range(n):
+        if_boom = False
+        if ar[i] == v:
+            for j in range(i+1, n):
+                if ar[j] == v and abs(j - i) <= k:
+                    if_boom = True
+                    break
+        if if_boom:
+            boom_count += 1
+    if max_boom_count < boom_count:
+        ans = v
+        max_boom_count = boom_count
+print(ans)
