647c41001582449413213d50345b79e7c408edaa
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-12 19:03:25 +0900

    Title: 초기 수열 복원하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250112/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(1, max(arr)+1):
+    possible_arr = []
+    success = True
+    possible_arr.append(i)
+    for j in range(len(arr)):
+        v = arr[j] - possible_arr[-1]
+        if v in possible_arr:
+            success = False
+        else:
+            possible_arr.append(v)
+    if success:        
+        for v in possible_arr:
+            print(v, end=' ')
+        exit(0)
