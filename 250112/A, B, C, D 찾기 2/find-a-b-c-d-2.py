aaaced2cf5ccbbce89489945b5486fade057112f
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-12 15:24:04 +0900

    Title: A, B, C, D 찾기 2, Time: 61ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250112/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 250112/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -9,7 +9,7 @@ for a in range(1, max(arr) + 1):
                 if a in arr and b in arr and c in arr and d in arr and \
                 a + b in arr and b + c in arr and c + d in arr and d + a in arr and \
                 a + c in arr and b + d in arr and \
-                a + b + c in arr and a + c + d in arr and b + c + d in arr and \
+                a + b + c in arr and  a + b + d in arr and a + c + d in arr and b + c + d in arr and \
                 a + b + c + d in arr:
                     print(a, b, c, d)
                     break
