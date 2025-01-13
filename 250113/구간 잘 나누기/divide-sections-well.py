b91a7d9bbffa5037cc8ced4785168c58095dc03d
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-13 20:14:27 +0900

    Title: 구간 잘 나누기, Time: 58ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250113/구간 잘 나누기/divide-sections-well.py
+++ 250113/구간 잘 나누기/divide-sections-well.py
@@ -1,26 +1,25 @@
-MAX_A = 10_000
-n, m = map(int, input().split())
+import sys
 
+n,m = map(int, input().split())
 a = list(map(int, input().split()))
 
-ans = MAX_A
-for i in range(1, MAX_A + 1):
+MAX_A = 10_000
+ans = sys.maxsize
+
+for i in range(MAX_A + 1): # 구간 합의 최댓값이 i일때, section 나누어지는 판단한다.
     possible = True
     section = 1
 
     cnt = 0
+
     for j in range(n):
         if a[j] > i:
             possible = False
             break
-        
         if cnt + a[j] > i:
             cnt = 0
             section += 1
-
         cnt += a[j]
-
-    if possible and section <= m:
-        ans = min (ans, i)
-
+    if possible and section == m:
+        ans = min(ans, i)
 print(ans)
