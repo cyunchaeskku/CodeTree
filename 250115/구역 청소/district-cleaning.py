ff19c5cb90a5e985f34fd378c6eb98506a483a7c
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-15 19:48:12 +0900

    Title: 구역 청소, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250115/구역 청소/district-cleaning.py
+++ 250115/구역 청소/district-cleaning.py
@@ -4,4 +4,10 @@ c, d = map(int, input().split())
 min_val = min(a, c)
 max_val = max(b, d)
 
-print(max_val - min_val)
+# print(max_val - min_val)
+
+ans = 0
+if b < c or d < a:
+    print( (b -a) + (d-c) )
+else:
+    print(max_val - min_val)
