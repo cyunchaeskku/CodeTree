d8bc1e9e21f251650c8c9220416091630a37722b
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-22 15:26:55 +0900

    Title: 코딩톡, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250122/코딩톡/coding-talk.py
+++ 250122/코딩톡/coding-talk.py
@@ -12,17 +12,14 @@ appeared_chrs = set()
 
 for i in range(m):
     c, u = input().split()
-    appeared_chrs.add(c)
     idx = ord(c) - 65
     if i >= p - 1:
         programmers[idx] = True
-    if i == p-1 and u == 0:
+    if i == p-1 and int(u) == 0:
         # for v in appeared_chrs:
         #     programmers[ord(v) - 65] = True
-        for v in programmers:
-            v = True
-
-
+        for j in range(len(programmers)):
+            programmers[j] = True
 
 for i, v in enumerate(programmers):
     if v == False:
