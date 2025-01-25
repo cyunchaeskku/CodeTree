a89b38358594859901c1b47218bffad02d3d9263
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-25 20:52:25 +0900

    Title: 움직이는 블록, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250125/움직이는 블록/moving-block.py
+++ 250125/움직이는 블록/moving-block.py
@@ -10,6 +10,19 @@ a = [
 avg = int(sum(a) / n)
 ans = 0
 
+
+for i in range(n):
+    if a[i] > avg:
+        val = a[i] - avg
+        a[i] -= val
+        a[a.index(min(a))] += val
+        ans += val
+    elif a[i] < avg:
+        val = avg - a[i]
+        a[i] += val
+        a[a.index(max(a))] -= val
+        ans += val
+
 for i in range(n):
     if a[i] > avg:
         val = a[i] - avg
