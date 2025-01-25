b25289c059154bcd2217c51069aa8b9aa68150fe
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-25 17:09:42 +0900

    Title: L, R 그리고 B, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250125/L, R 그리고 B/l-r-and-b.py
+++ 250125/L, R 그리고 B/l-r-and-b.py
@@ -23,7 +23,11 @@ for i in range(10):
 
 # L에서 B로 가는 경로에 R이 없을 경우의 답
 ans = abs( L_index[0] - B_index[0]) + abs( L_index[1] - B_index[1]) - 1
+
 # R이 가로막고 있으면 돌아가야 함
-if (L_index[0] == R_index[0] and R_index[0] == B_index[0]) or (L_index[1] == R_index[1] and R_index[1] == B_index[1]):
+if ( (L_index[0] == R_index[0] and R_index[0] == B_index[0]) or \
+    (L_index[1] == R_index[1] and R_index[1] == B_index[1]) ) and \
+    ((L_index[0] < R_index[0] and R_index[0] < B_index[0]) or (B_index[0] < R_index[0] and R_index[0] < L_index[0])) or \
+    ((L_index[1] < R_index[1] and R_index[1] < B_index[1]) or (B_index[1] < R_index[1] and R_index[1] < L_index[1])):
     ans += 2
 print(ans)
