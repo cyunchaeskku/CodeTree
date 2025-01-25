68a48f6e6a5f98db127025a4dedaa2012c676174
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-25 16:38:42 +0900

    Title: 순위 경쟁2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250125/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,40 @@
+import sys
+
+n = int(input())
+hall_of_fame = set()
+hall_of_fame.add('A')
+hall_of_fame.add('B')
+score = dict()
+score['A'] = 0
+score['B'] = 0
+
+cnt = 0
+
+for _ in range(n):
+    c, s = input().split()
+    s = int(s)
+    score[c] += s
+
+    if score['A'] > score['B']:
+        _hall_of_fame = ('A')
+        if hall_of_fame != _hall_of_fame:
+            cnt += 1
+        hall_of_fame = _hall_of_fame
+
+    elif score['A'] < score['B']:
+        _hall_of_fame = ('B')
+        if hall_of_fame != _hall_of_fame:
+            cnt += 1
+        hall_of_fame = _hall_of_fame
+        
+
+    elif score['A'] == score['B']:
+        _hall_of_fame = ('A', 'B')
+        if hall_of_fame != _hall_of_fame:
+            cnt += 1
+        hall_of_fame = _hall_of_fame
+        
+
+
+print(cnt)
+
