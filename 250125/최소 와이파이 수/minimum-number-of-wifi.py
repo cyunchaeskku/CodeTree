7ec6f8dca4c98c42f42cca4b54519e8bf38405d9
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-25 23:09:00 +0900

    Title: 최소 와이파이 수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250125/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,19 @@
+import sys
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+ans = 0
+
+wifi = [False for _ in range(n)]
+
+# m 거리까지는 wifi 사용 가능하므로.
+for i in range(0 + m, n - m):
+    if wifi[i] == True:
+        continue
+    if 1 in a[i-m:i+m+1]:
+        ans += 1
+        for j in range(i-m, i+m+1):
+            wifi[j] = True
+    
+print(ans)
