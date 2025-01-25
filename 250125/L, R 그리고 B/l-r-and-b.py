9eff4d7d1d9cdeefea5918bd0c979cfdaeef7129
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-25 17:11:07 +0900

    Title: L, R 그리고 B, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/L, R 그리고 B/l-r-and-b.py
+++ 250125/L, R 그리고 B/l-r-and-b.py
@@ -27,7 +27,7 @@ ans = abs( L_index[0] - B_index[0]) + abs( L_index[1] - B_index[1]) - 1
 # R이 가로막고 있으면 돌아가야 함
 if ( (L_index[0] == R_index[0] and R_index[0] == B_index[0]) or \
     (L_index[1] == R_index[1] and R_index[1] == B_index[1]) ) and \
-    ((L_index[0] < R_index[0] and R_index[0] < B_index[0]) or (B_index[0] < R_index[0] and R_index[0] < L_index[0])) or \
-    ((L_index[1] < R_index[1] and R_index[1] < B_index[1]) or (B_index[1] < R_index[1] and R_index[1] < L_index[1])):
+    ( ((L_index[0] < R_index[0] and R_index[0] < B_index[0]) or (B_index[0] < R_index[0] and R_index[0] < L_index[0])) or \
+    ((L_index[1] < R_index[1] and R_index[1] < B_index[1]) or (B_index[1] < R_index[1] and R_index[1] < L_index[1])) ):
     ans += 2
 print(ans)
