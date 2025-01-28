3b43e86053c427877ce95a89cdd1c08e587dad85
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-28 18:58:08 +0900

    Title: 연속되는 수 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250128/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,24 @@
+import sys
+
+n = int(input())
+
+a = [
+    int(input())
+    for _ in range(n)
+]
+
+cnt = 0
+ans  = -sys.maxsize
+
+for i in range(n):
+    if i > 0:
+        if a[i] != a[i - 1]:
+            ans = max(ans, cnt)
+            cnt = 1
+        else:
+            cnt += 1
+    else:
+        cnt += 1
+    
+
+print(ans)
