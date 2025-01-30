61eca9a9a660f4118d74e102d3b1e939da63f84b
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-30 15:38:50 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250130/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,36 @@
+import sys 
+
+N, K, P, T = map(int, input().split())
+
+input_string = []
+for i in range(T):
+    input_string.append(tuple(map(int, input().split())))
+
+input_string.sort()
+
+K_list = [K for _ in range(N+1)]
+K_list[0] = 0
+
+programmers = [0 for _ in range(N+1)]
+programmers[P] = 1
+
+
+for i in range(T):
+    t,x,y = input_string[i]
+    if programmers[x] == 1 and K_list[x] > 0: # x가 감염자일때
+        programmers[y] = 1
+        K_list[x] -= 1
+    if programmers[y] == 1 and K_list[y] > 0:
+        programmers[x] = 1
+        K_list[y] -= 1
+
+
+
+
+
+# ----- print answer ---------#
+for i,v in enumerate(programmers):
+    if i == 0:
+        continue
+    else:
+        print(v,end='')
