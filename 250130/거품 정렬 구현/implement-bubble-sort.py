8ead143274ec37838543550ae974619079ef0f47
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-30 23:05:29 +0900

    Title: 거품 정렬 구현, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250130/거품 정렬 구현/implement-bubble-sort.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(input())
+
+a = list(map(int, input()))
+
+def bubble_sort(a, n):
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+    return a
+
+
+ar = bubble_sort(a, n)
+for v in ar:
+    print(v, end=' ')
