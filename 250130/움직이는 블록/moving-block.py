be02efd35fcb3fe34f189e8d850ae219e57d785d
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-30 13:57:36 +0900

    Title: 움직이는 블록, Time: 80ms, Memory: 18MB, Status: Passed - CodeTree

--- 250130/움직이는 블록/moving-block.py
+++ 250130/움직이는 블록/moving-block.py
@@ -11,16 +11,5 @@ ans = 0
 
 for i in range(n):
     if a[i] > avg:
-        for j in range(a[i] - avg + 1):
-            if a[i] <= avg:
-                break
-            a[i] -= 1
-            ans += 1
-            for k in range(n):
-                if k == i:
-                    continue
-                elif a[k] < avg:
-                    a[k] += 1
-                    break
-
+        ans += a[i] - avg
 print(ans)
