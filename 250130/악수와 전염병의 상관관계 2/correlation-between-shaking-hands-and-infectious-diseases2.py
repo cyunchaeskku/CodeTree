ad2b409ad720d75e19c118191ae9c4cedde57a05
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2025-01-30 15:45:44 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250130/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250130/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -17,7 +17,7 @@ programmers[P] = 1
 
 for i in range(T):
     t,x,y = input_string[i]
-    if programmers[x] == 1 and K_list[x] > : # x가 감염자일때
+    if programmers[x] == 1 and K_list[x] > 0: # x가 감염자일때
         programmers[y] = 1
         K_list[x] -= 1
     if programmers[y] == 1 and K_list[y] > 0:
